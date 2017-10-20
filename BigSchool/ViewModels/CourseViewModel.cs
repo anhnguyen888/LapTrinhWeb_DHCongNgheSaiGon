43d9ffa8098f4fa8f224882c701f152b85f7c28c
Student <Student@L2201>
2017-10-20 10:38:49 +0700

    Add Validate Client

--- BigSchool/ViewModels/CourseViewModel.cs
+++ BigSchool/ViewModels/CourseViewModel.cs
@@ -9,14 +9,18 @@ namespace BigSchool.ViewModels
 {
     public class CourseViewModel
     {
+        [Required(ErrorMessage ="Place is Required")]
         public string Place { get; set; }
 
+        [Required(ErrorMessage = "Date is Required")]
         public string Date { get; set; }
 
+        [Required(ErrorMessage = "Time is Required")]
         public string Time { get; set; }
 
         public IEnumerable<Category> Categories { get; set; }
 
+        [Required(ErrorMessage = "Category is Required")]
         [Display(Name ="Category")]
         public int CategoryId { get; set; }
     }
