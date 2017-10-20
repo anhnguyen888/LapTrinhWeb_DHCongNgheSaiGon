f6802a89b9774e870aa2a9785b111268b3e7be40
Student <Student@L2201>
2017-10-20 10:10:02 +0700

    Lab 4,5,6 - Practice folow Lecturer

---
+++ BigSchool/Controllers/CoursesController.cs
@@ -0,0 +1,27 @@
+﻿using BigSchool.Models;
+using BigSchool.ViewModels;
+using System.Web.Mvc;
+using System.Data.Entity;
+
+namespace BigSchool.Controllers
+{
+    public class CoursesController : Controller
+    {
+        private readonly ApplicationDbContext dbContext;
+
+        public CoursesController()
+        {
+            dbContext = new ApplicationDbContext();
+        }
+
+        public ActionResult Create()
+        {
+            var viewModel = new CourseViewModel()
+            {
+                Categories = dbContext.Categories
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ BigSchool/ViewModels/CourseViewModel.cs
@@ -0,0 +1,23 @@
+﻿using BigSchool.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace BigSchool.ViewModels
+{
+    public class CourseViewModel
+    {
+        public string Place { get; set; }
+
+        public string Date { get; set; }
+
+        public string Time { get; set; }
+
+        public IEnumerable<Category> Categories { get; set; }
+
+        [Display(Name ="Category")]
+        public int CategoryId { get; set; }
+    }
+}
